ec58dee20f05f47d552bc09888f4b91570260f2a
jsilva83 <jorge.h.silva@gmail.com>
2022-07-08 11:05:11 +0100

    - Wrong file.

--- dashboard_graph.py
+++ dashboard_graph.py
@@ -412,36 +412,3 @@ class DrawDashboard:
                     + current_date.strftime("%Y%m%d%H%M%S") + ').svg'
         plot.savefig(file_path, dpi=200, format='svg')
         return
-
-"""
-import matplotlib.pyplot as plt
-  
-# Setting labels for items in Chart
-Employee = ['Roshni', 'Shyam', 'Priyanshi', 'Harshit', 'Anmol']
-  
-# Setting size in Chart based on 
-# given values
-Salary = [40000, 50000, 70000, 54000, 44000]
-  
-# colors
-colors = ['#FF0000', '#0000FF', '#FFFF00', '#ADFF2F', '#FFA500']
-
-# explosion
-explode = (0.05, 0.05, 0.05, 0.05, 0.05)
-  
-# Pie Chart
-plt.pie(Salary, colors=colors, labels=Employee, autopct='%1.1f%%', pctdistance=0.85, explode=explode)
-  
-# draw circle
-centre_circle = plt.Circle((0, 0), 0.70, fc='white')
-fig = plt.gcf()
-  
-# Adding Circle in Pie chart
-fig.gca().add_artist(centre_circle)
-  
-# Adding Title of chart
-plt.title('Employee Salary Details')
-  
-# Displaying Chart
-plt.show()
-"""
